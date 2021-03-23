15670d2798f7b39fca840d0ae43aa051c154a300
ShreeDhushandhan <dhushandhan.sd@gmail.com>
2021-03-23 11:37:00 +0530

    Small Js scripts are practiced

--- logics.js
+++ logics.js
@@ -2,3 +2,8 @@ const oddOrEven = (number) => {
   const arr = ['EVEN','ODD'];
   document.getElementById('output').innerHTML = arr[parseInt(number)%2];
 }
+
+const protoctor = () => {
+  document.getElementById('output').innerHTML = 'Get Out';
+  alert('Dont try to touch the Box');
+}
